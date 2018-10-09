8cf23a5e1a6616f40110da900b35c478cadf1797
karma <kolenkoal@mail.ru>
2018-10-10 00:39:05 +0300

    Refactored scss-code

--- modal.js
+++ modal.js
@@ -5,8 +5,10 @@ function ModalService (options) {
 		modal.innerHTML = this.options.template;
 		modal.setAttribute('style', 'top: 35%');
 		// // icon for modal-window closing
-		// var closeIcon = document.getElementsByClassName('btn close-popup');
+		// var closeIcon = document.createElement('button');
+		// closeIcon.classList.add('btn close-popup');
 		// closeIcon.onclick = this.close.bind(this);
+		// modal.appendChild(closeIcon);
 	};
 
 	this.close = function () {
